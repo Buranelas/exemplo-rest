dd91261048211ccfc183a721f52910a571409b83
João Vitor <fiuknelas@gmail.com>
2024-05-07 20:34:32 -0300

    Adicionado CRUD completo

--- src/main/java/repositories/ClienteRepository.java
+++ src/main/java/repositories/ClienteRepository.java
@@ -10,6 +10,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import models.Cliente;
 
 /**
@@ -20,6 +22,19 @@ public class ClienteRepository {
     
     private static final String INSERT = 
             "INSERT INTO CLIENTE(ID, NOME, CPF) VALUES(?, ?, ?)";
+    
+    private static final String UPDATE =
+            "UPDATE CLIENTE SET NOME = ? AND CPF ? WHERE ID = ?";
+    
+    private static final String DELETE =
+            "DELETE CLIENTE WHERE ID = ?";
+    
+    private static final String FIND_ALL =
+            "SELECT * FROM CLIENTE";
+    
+    private static final String FIND_BY_ID =
+            "SELECT * FROM CLIENTE WHERE ID = ?";
+    
     
     public Cliente insert(Cliente cliente) throws SQLException{
         
@@ -50,6 +65,107 @@ public class ClienteRepository {
         }
         return cliente;
         
+    }
+    
+    public Cliente update(Cliente cliente) throws SQLException{
+        
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        
+        try{
+            connection = new ConnectionFactory().getConnection();
+            
+            pstmt = connection.prepareStatement(UPDATE);
+            pstmt.setString(1, cliente.getNome());
+            pstmt.setString(2, cliente.getCpf());
+            pstmt.setInt(3, cliente.getId());
+            pstmt.executeUpdate();
+            
+        } finally{
+            
+            if (pstmt != null)
+                pstmt.close();
+            if (connection != null)
+                connection.close(); 
+        }
+        return cliente;
+    }
+    
+    public void delete(int id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        
+        try {
+            connection = new ConnectionFactory().getConnection();
+            pstmt = connection.prepareStatement(DELETE);
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null)
+                pstmt.close();
+            if (connection != null)
+                connection.close();
+        }
+    }
+    
+     public List<Cliente> findAll() throws SQLException {
+        List<Cliente> clientes = new ArrayList<>();
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        
+        try {
+            connection = new ConnectionFactory().getConnection();
+            pstmt = connection.prepareStatement(FIND_ALL);
+            rs = pstmt.executeQuery();
+            
+            while (rs.next()) {
+                Cliente cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNome(rs.getString("nome"));
+                cliente.setCpf(rs.getString("cpf"));
+                clientes.add(cliente);
+            }
+        } finally {
+            if (rs != null)
+                rs.close();
+            if (pstmt != null)
+                pstmt.close();
+            if (connection != null)
+                connection.close();
+        }
+        
+        return clientes;
+    }
+     
+      public Cliente findById(int id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        
+        try {
+            connection = new ConnectionFactory().getConnection();
+            pstmt = connection.prepareStatement(FIND_BY_ID);
+            pstmt.setInt(1, id);
+            rs = pstmt.executeQuery();
+            
+            if (rs.next()) {
+                Cliente cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNome(rs.getString("nome"));
+                cliente.setCpf(rs.getString("cpf"));
+                return cliente;
+            }
+        } finally {
+            if (rs != null)
+                rs.close();
+            if (pstmt != null)
+                pstmt.close();
+            if (connection != null)
+                connection.close();
+        }
+        
+        return null;
     }
     
 }
--- src/main/java/service/ClienteService.java
+++ src/main/java/service/ClienteService.java
@@ -6,6 +6,7 @@ package service;
 
 import exceptions.ValidacaoException;
 import java.sql.SQLException;
+import java.util.List;
 import javax.naming.NamingException;
 import models.Cliente;
 import repositories.ClienteRepository;
@@ -34,5 +35,47 @@ public class ClienteService {
         
         return new ClienteRepository().insert(cliente);
         
+    }
+    
+    public Cliente update(Cliente cliente) throws 
+            ValidacaoException, SQLException, NamingException{
+        
+        if(cliente.getCpf().isEmpty()){
+            throw new ValidacaoException("CPF não pode ser nulo");
+        }
+        if(cliente.getCpf().trim().length() != 11){
+            throw new ValidacaoException("Valor de CPF inválido");
+        }
+        
+        if(cliente.getNome().trim().isEmpty()){
+            throw new ValidacaoException("Nome não pode ser nulo");
+        }
+        
+        
+        return new ClienteRepository().update(cliente);
+        
+    }
+    
+    public void delete(int id) throws 
+            ValidacaoException, SQLException, NamingException{
+        
+        if(id == 0)
+            throw new ValidacaoException("Informe o Cliente a ser Deletado");
+        
+        new ClienteRepository().delete(id);
+        
+    }
+    
+    public List<Cliente> findAll() throws 
+            ValidacaoException, SQLException, NamingException{
+        
+        return new ClienteRepository().findAll();
+        
+    }
+    
+    public Cliente findById(int id) throws
+            ValidacaoException, SQLException, NamingException{
+        
+        return new ClienteRepository().findById(id);
     }
 }
